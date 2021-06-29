d6206ae762292e81f332715c2b02d9e72cfae9ed
brentshierk <34776645+brentshierk@users.noreply.github.com>
2021-06-28 18:08:21 -0700

    re writing program

--- makesite.go
+++ makesite.go
@@ -6,108 +6,50 @@ import (
 	"html/template"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-type Page struct{
-	TextFilePath string
-	TextFileName string
-	HTMLPagePath string
-	Content string
-}
+
 
 func main() {
-	var TextFilePath,directory string
-	flag.StringVar(&TextFilePath,"file","","text file you want to turn into an html page")
-	flag.StringVar(&directory,"dir","","direcotry to look for text files to convert (all)")
+	var fileName,directory string
+
+	flag.StringVar(&fileName,"file","","text file you want to turn into an html page")
+	flag.StringVar(&directory,"dir",".","directory to look for text files to convert (all)")
 	flag.Parse()
 
-	if TextFilePath == "" && directory ==""{
-		panic("missing the --flag or --dir flag please suply one or the other")
-
-	}
+	
 
 	if directory != ""{
-		createallFiles(directory)
-	}
-
-	newPage := createPageFromTextFile(TextFilePath)
-
-	renderTemplateFromPage("template.tmpl",newPage)
-
-}
-
-func renderTemplateFromPage(templateFilePath string,page Page){
-	t:= template.Must(template.New(templateFilePath).ParseFiles(templateFilePath))
-
-	newFile,err := os.Create(page.HTMLPagePath)
-	if err != nil{
-		panic(err)
+		dirToHtml(directory)
+	}else if fileName != ""{
+		fileToHtml(fileName)
 
 	}
 
-	t.Execute(newFile,page)
-	fmt.Println("generated file :",page.HTMLPagePath)
-}
-func createallFiles(directory string,)(page Page){
 	
-	files,err := os.ReadDir(directory)
-
-	if err != nil{
-		print(err)
-	}
-
-	for _,file := range files{
-		path := directory + "/" +file.Name()
-
-		info,err := os.Stat(path)
-		if err !=nil{
-			println(err)
-		}
-
-		if info.IsDir(){
-			createallFiles(path)
-		}else {
-			if isTxt(file.Name()) {
-				fileContent := readFile(path)
-				page:= createPageFromTextFile("template.tmpl" + path+string(fileContent))
-				println(page)
-
-			}
-		}
-	}
-
-	return page
 }
-func readFile(file string) []byte {
-	content, err := os.ReadFile(file)
-	if err !=nil{
+
+func fileToHtml(fileName string){
+	fileContent, err := ioutil.ReadFile(fileName)
+	checkErr(err)
+
+	f,err := os.Create(strings.SplitN(fileName,".",2)[0]+".html")
+	checkErr(err)
+
+	t:= template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	err = t.Execute(f,string(fileContent))
+	checkErr(err)
+
+	f.Close()
+}
+
+
+func checkErr(err error){
+	if err != nil{
 		println(err)
 	}
-	return content
-}
-func isTxt(filename string) bool {
-	fileExt := filename[len(filename)-4:]
-	return fileExt == ".txt"
-}
-
-func createPageFromTextFile(filePath string)Page{
-	fileContents , err := ioutil.ReadFile(filePath)
-	if err != nil{
-		panic(err)
-	}
-
-
-
-	fileNameWithoutExtension := strings.Split(filePath,".txt")[0]
-
-	return Page{
-		TextFilePath: filePath,
-		TextFileName: fileNameWithoutExtension,
-		HTMLPagePath: fileNameWithoutExtension + ".html",
-		Content: string(fileContents),
-	}
-
 }
 
 
