ec8ca997e6a422d15d4433f18f269e29a201d0f0
brentshierk <34776645+brentshierk@users.noreply.github.com>
2021-06-30 12:50:45 -0700

    directory files to html fuction working, working on spotify api function

--- makesite.go
+++ makesite.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	//"fmt"
 	"html/template"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	//"path/filepath"
 	"strings"
+	"spotify"
+	"http"
 )
 
 
@@ -16,7 +18,7 @@ func main() {
 	var fileName,directory string
 
 	flag.StringVar(&fileName,"file","","text file you want to turn into an html page")
-	flag.StringVar(&directory,"dir",".","directory to look for text files to convert (all)")
+	flag.StringVar(&directory,"directory",".","directory to look for text files to convert (all)")
 	flag.Parse()
 
 	
@@ -54,6 +56,34 @@ func dirToHtml(directory string){
 		}
 
 	}
+
+}
+
+func makePlaylist(file string){
+ClientID := "32d484634e5843ee80024a0d65d1459d"
+ClientSecret := "4629c73660b8499fa4809255e4bba77e"
+auth.SetAuthInfo(clientID, secretKey)
+
+//request_url := "https://api.spotify.com/v1/search?q="+track_name+"&type=track&market=US&limit=1&offset=5"
+//var track_name string 
+
+//read in the file 
+fileContent, err := ioutil.ReadFile(fileName)
+	checkErr(err)
+//split the content of the file at ","
+// for each substring compleate a query to the spotify api
+//return json of playlist
+//get  playlist icon,title,link to playlist
+
+
+
+results, err := client.Search(song string , spotify.SearchTypePlaylist|spotify.SearchTypeAlbum)
+	checkErr(err)
+
+
+
+
+
 
 }
 
